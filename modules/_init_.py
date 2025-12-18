1455f40fdf3b79b1e5545943210a335f81434925
hasbleidyt003 <hasbleidyt003@gmail.com>
2025-12-18 07:58:16 -0500

    Update _init_.py

--- modules/_init_.py
+++ modules/_init_.py
@@ -1,18 +1,4 @@
+# modules/__init__.py
 """
-Módulos del Sistema de Gestión de Cupos - Tododrogas
-
-Contiene:
-- database: Funciones de base de datos
-- auth: Autenticación y seguridad
-- utils: Utilidades generales
+Módulos del sistema de autorización parcial de OCs
 """
-
-__version__ = "1.0.0"
-__author__ = "Equipo Tododrogas"
-
-# Importaciones principales
-from .database import init_db, get_clientes, get_ocs_pendientes, crear_oc, autorizar_oc
-from .auth import authenticate, create_user, get_users, change_password
-from .utils import format_currency, calculate_percentage, validate_nit
-
-__all__ = ['database', 'auth', 'utils']
